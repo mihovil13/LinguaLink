9c38dadb54a2c6b03843d5ddefb4af066e9c7364
djurgec <96839627+djurgec@users.noreply.github.com>
2024-11-14 00:56:00 +0100

    azuriran cors

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -29,13 +29,4 @@ public class DemoApplication {
 		};
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
 }
