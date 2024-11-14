517a9c553edf1bd2c04a2d05b77a98efbd7cf967
asttajku <adi.stajku@fer.unizg.hr>
2024-11-15 00:04:25 +0100

    Preuređen CSS + sređen prikaz učitelja

--- IzvorniKod/frontend/src/Components/TeacherList/TeacherList.jsx
+++ IzvorniKod/frontend/src/Components/TeacherList/TeacherList.jsx
@@ -1,54 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./TeacherList.css";
-import person from "../Assets/person.png"
+import axios from "axios";
 
 const TeacherList = () => {
 
+    // definiramo podatke 
+    const [teachers, setTeachers] = useState([]);
 
+    useEffect(() => {
+        const fetchTeacherData = async () => {
+            try {
+                const response = await axios.get("http://localhost:8080/teachers", {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },  
+                });
+                if (response.status === 200) {
+                    setTeachers(response.data);
+                }
+            } catch (error) {
+                console.error("Error fetching teacher list:", error);
+                alert("Došlo je do greške prilikom dohvaćanja liste učitelja.");
+            }
+        };
+
+        fetchTeacherData();
+    }, []);
     return (
         <div className="container">
-            <div className="teacher-container">
-                <img src={person} alt="" />
-                <div className="tekst">
-                <p>Ivan Pavao</p>
-                </div>
+            {teachers.map((teacher, index) => (
+            <div className="teacher-container" key={index}>
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg" alt="" />
+                <p>{teacher.ime} {teacher.prezime}</p>
             </div>
-            <div className="teacher-container">
-                <img src={person} alt="" />
-                <div className="tekst">
-                <p>Ivan Pavao</p>
-                </div>
-            </div>
-            <div className="teacher-container">
-                <img src={person} alt="" />
-                <div className="tekst">
-                <p>Ivan Pavao</p>
-                </div>
-            </div>
-            <div className="teacher-container">
-                <img src={person} alt="" />
-                <div className="tekst">
-                <p>Ivan Pavao</p>
-                </div>
-            </div>
-            <div className="teacher-container">
-                <img src={person} alt="" />
-                <div className="tekst">
-                <p>Ivan Pavao</p>
-                </div>
-            </div>
-            <div className="teacher-container">
-                <img src={person} alt="" />
-                <div className="tekst">
-                <p>Ivan Pavao</p>
-                </div>
-            </div>
-            <div className="teacher-container">
-                <img src={person} alt="" />
-                <div className="tekst">
-                <p>Ivan Pavao</p>
-                </div>
-            </div>
+        ))}
         </div>
     );
 
