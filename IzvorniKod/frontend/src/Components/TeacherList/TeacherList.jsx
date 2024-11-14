8daa0e672fe4062cde68121e1f90b5eb399d524c
asttajku <adi.stajku@fer.unizg.hr>
2024-11-14 23:03:53 +0100

    Slozena stranica TeacherList - bez funkcionalnosti

--- IzvorniKod/frontend/src/Components/TeacherList/TeacherList.jsx
+++ IzvorniKod/frontend/src/Components/TeacherList/TeacherList.jsx
@@ -1,13 +1,54 @@
 import React from "react";
 import "./TeacherList.css";
+import person from "../Assets/person.png"
 
 const TeacherList = () => {
 
 
     return (
         <div className="container">
-
-
+            <div className="teacher-container">
+                <img src={person} alt="" />
+                <div className="tekst">
+                <p>Ivan Pavao</p>
+                </div>
+            </div>
+            <div className="teacher-container">
+                <img src={person} alt="" />
+                <div className="tekst">
+                <p>Ivan Pavao</p>
+                </div>
+            </div>
+            <div className="teacher-container">
+                <img src={person} alt="" />
+                <div className="tekst">
+                <p>Ivan Pavao</p>
+                </div>
+            </div>
+            <div className="teacher-container">
+                <img src={person} alt="" />
+                <div className="tekst">
+                <p>Ivan Pavao</p>
+                </div>
+            </div>
+            <div className="teacher-container">
+                <img src={person} alt="" />
+                <div className="tekst">
+                <p>Ivan Pavao</p>
+                </div>
+            </div>
+            <div className="teacher-container">
+                <img src={person} alt="" />
+                <div className="tekst">
+                <p>Ivan Pavao</p>
+                </div>
+            </div>
+            <div className="teacher-container">
+                <img src={person} alt="" />
+                <div className="tekst">
+                <p>Ivan Pavao</p>
+                </div>
+            </div>
         </div>
     );
 
