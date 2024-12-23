00927351347f00602a9b15378748d1e9fa6668a6
asttajku <adi.stajku@fer.unizg.hr>
2024-12-24 00:05:50 +0100

    Dodana opcija uređivanja profila
    Složen gumb za uređivanje profila, izbornici za dodavanje jezika koje znamo i koje želimo naučiti, napravljene i frontend funkcije koje bi ispravno trebale prikazat podatke kada se povežu s backendom

--- IzvorniKod/frontend/src/Components/Profile/ProfilePage.jsx
+++ IzvorniKod/frontend/src/Components/Profile/ProfilePage.jsx
@@ -12,9 +12,12 @@ const ProfilePage = () => {
     prezime: "",
     email: "",
     uloga: "",
-    languagesKnown: ["Engleski"],
-    languagesToLearn: ["Francuski"],
+    languagesKnown: [{language: "", level: ""}],
+    languagesToLearn: [{language: "", level: ""}],
   });
+
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+  const [editedUser, setEditedUser] = useState(user);
 
   // useEffect je hook koji upravlja stvarima poput dohvacanja podataka, manipulacije DOM-a itd...
   //sastoji se od funkcije, i od polja ovisnosti koje nareduje kada ce se funkcija izvrsiti
@@ -29,7 +32,7 @@ const ProfilePage = () => {
         });
 
         if (response.status === 200) {
-          const { ime, prezime, email, uloga } = response.data; //iz odgovora uzimamo navedene varijable
+          const { ime, prezime, email, uloga, languagesKnown, languagesToLearn } = response.data; //iz odgovora uzimamo navedene varijable
 
           // azuriramo podatke
           setUser({
@@ -37,8 +40,8 @@ const ProfilePage = () => {
             prezime: prezime || "",
             email: email || "",
             uloga: uloga || "",
-            languagesKnown: ["Engleski"],
-            languagesToLearn: ["Francuski"],
+            languagesKnown: languagesKnown || [],
+            languagesToLearn: languagesToLearn || [],
           });
         }
       } catch (error) {
@@ -49,6 +52,50 @@ const ProfilePage = () => {
 
     fetchUserProfile(); //pozivanje funkcije koja se nalazi unutar hooka
   }, []);
+
+  const handleEditProfile = () => {
+    setEditedUser(user); // postavlja trenutno stanje korisnika u stanje uredivanja
+    setEditModalOpen(true); // otvara modal
+  }
+
+  const handleSaveProfile = async () => {
+    try {
+      const response = await axios.put("http://localhost:8080/api/moj-profil", editedUser,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+          }
+        }
+      )
+
+      if (response.data === 200) {
+        setUser(editedUser);
+        setEditModalOpen(false);
+        alert("Profil uspjesno spremljen");
+      } else {
+        alert("Doslo je do greske prilikom spremanja profila");
+      }
+    } catch (error) {
+      console.error("Error saving profile:", error);
+      alert("Doslo je do pogreske prilikom spremanja profila");
+    }
+  }
+
+  const handleInputChange = (field, index, value, listType) => {
+    const updatedList = [...editedUser[listType]]; // kopira listu iz objekta editedUser
+    updatedList[index][field] = value;
+    setEditedUser({...editedUser, [listType]: updatedList });
+  }
+
+  const handleAddLanguage = (listType) => {
+    const updatedList = [...editedUser[listType], { language: "", level: "početna" }];
+    setEditedUser({...editedUser, [listType]: updatedList });
+  }
+
+  const handleRemoveLanguage = (index, listType) => {
+    const updatedList = editedUser[listType].filter((_, i) => i !== index); // izostavlja element na danom indexu
+    setEditedUser({...editedUser, [listType]: updatedList });
+  }
 
   return (
     <div className="profile-page">
@@ -72,14 +119,14 @@ const ProfilePage = () => {
           <span>Jezici koje znam:</span>
           <ul>
             {user.languagesKnown.map((lang, index) => (
-              <li key={index}>{lang}</li>
+              <li key={index}>{lang.language} - {lang.level}</li>
             ))}
           </ul>
 
           <span>Jezici koje želim naučiti:</span>
           <ul>
             {user.languagesToLearn.map((lang, index) => (
-              <li key={index}>{lang}</li>
+              <li key={index}>{lang.language} - {lang.level}</li>
             ))}
           </ul>
         </div>
@@ -97,11 +144,83 @@ const ProfilePage = () => {
           <h1 className="profile-name">{user.ime}</h1>
           <h1 className="profile-surname">{user.prezime}</h1>
         </div>
-        <div className="profile-buttons">
-          <button className="teachers-button" onClick={() => navigate("/teachers")}>Prikaz učitelja</button>
-          <button className="odjava-button" onClick={() => navigate("/")}>Odjava</button>
+        <div className="buttons">
+          <div className="edit-button">
+            <button className="edit-profile-button" onClick={handleEditProfile}>Uredi profil</button>
+          </div>
+          <div className="profile-buttons">
+            <button className="teachers-button" onClick={() => navigate("/teachers")}>Prikaz učitelja</button>
+            <button className="odjava-button" onClick={() => navigate("/")}>Odjava</button>
+          </div>
         </div>
       </div>
+      {isEditModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <div className="jezici-koje-znam">
+              <h3>Jezici koje znam</h3>
+              {editedUser.languagesKnown.map((lang, index) => (
+                <div key={index}>
+                  <input
+                    type="text"
+                    value={lang.language}
+                    onChange={(e) =>
+                      handleInputChange("language", index, e.target.value, "languagesKnown")
+                    }
+                    placeholder="Jezik"
+                  />
+                  <select
+                    value={lang.level}
+                    onChange={(e) =>
+                      handleInputChange("level", index, e.target.value, "languagesKnown")
+                    }
+                  >
+                    <option value="početna">Početna</option>
+                    <option value="srednja">Srednja</option>
+                    <option value="napredna">Napredna</option>
+                  </select>
+                  <button onClick={() => handleRemoveLanguage(index, "languagesKnown")}>Ukloni</button>
+                </div>
+              ))}
+              <button onClick={() => handleAddLanguage("languagesKnown")}>Dodaj Jezik</button>
+            </div>
+
+            <div className="jezici-koje-zelim-nauciti">
+              <h3>Jezici koje želim naučiti</h3>
+              {editedUser.languagesToLearn.map((lang, index) => (
+                <div key={index}>
+                  <input
+                    type="text"
+                    value={lang.language}
+                    onChange={(e) =>
+                      handleInputChange("language", index, e.target.value, "languagesToLearn")
+                    }
+                    placeholder="Jezik"
+                  />
+                  <select
+                    value={lang.level}
+                    onChange={(e) =>
+                      handleInputChange("level", index, e.target.value, "languagesToLearn")
+                    }
+                  >
+                    <option value="početna">Početna</option>
+                    <option value="srednja">Srednja</option>
+                    <option value="napredna">Napredna</option>
+                  </select>
+                  <button onClick={() => handleRemoveLanguage(index, "languagesToLearn")}>
+                    Ukloni
+                  </button>
+                </div>
+              ))}
+              <button onClick={() => handleAddLanguage("languagesToLearn")}>Dodaj Jezik</button>
+            </div>
+            <div className="spremi-zatvori">
+              <button onClick={handleSaveProfile}>Spremi</button>
+              <button onClick={() => setEditModalOpen(false)}>Zatvori</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
