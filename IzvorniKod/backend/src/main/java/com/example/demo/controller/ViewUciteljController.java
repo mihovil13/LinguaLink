03c04877363843d0d35369ff43f998ddde50a747
Mihovil Mađerić <mihovil.maderic@fer.hr>
2025-01-08 17:57:30 +0100

    Create ViewUciteljController.java

---
+++ IzvorniKod/backend/src/main/java/com/example/demo/controller/ViewUciteljController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Ucitelj;
+import com.example.demo.service.UciteljServiceJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ucitelj")
+public class ViewUciteljController {
+
+    @Autowired
+    private final UciteljServiceJPA uciteljService;
+
+    public ViewUciteljController(UciteljServiceJPA uciteljService) {
+        this.uciteljService = uciteljService;
+    }
+
+    // Endpoint za učitavanje profila učitelja
+    @GetMapping("/{id}")
+    public Ucitelj getUcitelj(@PathVariable long id) {
+        Ucitelj ucitelj = uciteljService.getUciteljById(id);
+        System.out.println(ucitelj);
+        return ucitelj;
+    }
+}
