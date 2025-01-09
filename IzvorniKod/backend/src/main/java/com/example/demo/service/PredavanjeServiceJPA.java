4c25f4ae20be767bee9b7bcea29db1b70615cc18
Mihovil Mađerić <mihovil.maderic@fer.hr>
2025-01-09 13:31:08 +0100

    Create PredavanjeServiceJPA.java

---
+++ IzvorniKod/backend/src/main/java/com/example/demo/service/PredavanjeServiceJPA.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Predavanje;
+import com.example.demo.repository.PredavanjeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PredavanjeServiceJPA implements PredavanjeService {
+
+    private final PredavanjeRepository predavanjeRepository;
+
+    @Autowired
+    public PredavanjeServiceJPA(PredavanjeRepository predavanjeRepository) {
+        this.predavanjeRepository = predavanjeRepository;
+    }
+
+    @Override
+    public Predavanje getPredavanjeById(int id) {
+        return predavanjeRepository.getPredavanjeById(id);
+    }
+
+    @Override
+    public void savePredavanje(Predavanje predavanje) {
+        predavanjeRepository.save(predavanje);
+    }
+
+    @Override
+    public List<Predavanje> getPredavanjaByUciteljId(int uciteljId) {
+        return predavanjeRepository.findByUciteljId(uciteljId);
+    }
+}
