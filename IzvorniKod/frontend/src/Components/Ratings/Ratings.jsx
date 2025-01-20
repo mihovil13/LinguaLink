6d74d24b445f373f6ca60cdf5709295028d8cf58
Mihovil Mađerić <mihovil.maderic@fer.hr>
2025-01-20 10:01:43 +0100

    Update Ratings.jsx

--- IzvorniKod/frontend/src/Components/Ratings/Ratings.jsx
+++ IzvorniKod/frontend/src/Components/Ratings/Ratings.jsx
@@ -6,7 +6,7 @@ import "./Ratings.css";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
-const backend = "http://localhost:8080";
+const backend = "https://lingualink-rxoa.onrender.com";
 
 const Ratings = () => {
   const { user, setUser } = useUser();
